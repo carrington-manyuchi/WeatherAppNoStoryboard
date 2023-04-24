a77055b7102d2f942bed0862b036c8a83f0173c7
Carrington <Manyuchi>
2023-04-24 22:11:38 +0200

    hourly collection View Cell Data

--- WeatherAppMay/View/HourlyTableViewCell.swift
+++ WeatherAppMay/View/HourlyTableViewCell.swift
@@ -25,3 +25,24 @@ class HourlyTableViewCell: UICollectionViewCell {
         collectionView.reloadData()
     }
 }
+
+
+
+extension HourlyTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  
+    //MARK: - Basic CollectionView Functionality
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return hourly.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherCollectionViewCell.identifier, for: indexPath) as! WeatherCollectionViewCell
+        cell.configure(with: hourly[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+    
+}
